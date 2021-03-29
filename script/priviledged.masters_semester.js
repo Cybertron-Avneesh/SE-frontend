21f24c28f6031f21f4886330310e8a466e6c4aa9
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2021-03-30 00:06:37 +0530

    Connects UI to backend for Semester

--- script/priviledged.masters_semester.js
+++ script/priviledged.masters_semester.js
@@ -3,71 +3,130 @@ let semesters = [];
 function addSemester() {
 
     var semester = {
-        branch_ID: document.getElementById('createBranchID'),
-        sem_num: document.getElementById('createSumNum'),
-        sem_ID: document.getElementById('createSemID')
+        branch_id: document.getElementById('createBranchID').value,
+        semester_num: document.getElementById('createSumNum').value,
+        semester_id: document.getElementById('createSemID').value,
+        my_id: "IIB2019050",
+        my_level: 2,
+        name: "op3"
     }
+    
+    var options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(semester)
+    }
+
+    fetch('http://localhost:5440/masters/semester?action=1', options)
+        .then(res => {
+            res.json()
+                .then(data => {
+                    console.log(data)
+                    window.alert("Semester created sucessfully.")
+                })
+                .catch(err => console.log(`${err}`))
+        })
+        .catch(err => console.error(err))
+
 
     semesters.push(semester);
     document.forms[0].reset();
 }
 
-function updateSemester() {
+// function updateSemester() {
 
-    var semester = {
-        branch_ID: document.getElementById('upBranchID'),
-        sem_num: document.getElementById('upSumNum'),
-        sem_ID: document.getElementById('upSemID')
-    }
+//     var semester = {
+//         branch_ID: document.getElementById('upBranchID'),
+//         sem_num: document.getElementById('upSumNum'),
+//         sem_ID: document.getElementById('upSemID')
+//     }
 
-    semesters.push(semester);
-    document.forms[0].reset();
-}
+//     semesters.push(semester);
+//     document.forms[0].reset();
+// }
 
 function deleteSemester() {
 
     var semester = {
-        branch_ID: document.getElementById('delBranchID'),
-        sem_num: document.getElementById('delSumNum'),
-        sem_ID: document.getElementById('delSemID')
+        semester_id: document.getElementById('delSemID').value,
+        my_id: "IIB2019050",
+        my_level: 2,
+        name: "op3"
     }
+    
+    var options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(semester)
+    }
+
+    fetch('http://localhost:5440/masters/semester?action=4', options)
+        .then(res => {
+            res.json()
+                .then(data => {
+                    console.log(data)
+                    window.alert("Semester deleted sucessfully.")
+                })
+                .catch(err => console.log(`${err}`))
+        })
+        .catch(err => console.error(err))
 
     semesters.push(semester);
     document.forms[0].reset();
 }
 
-let baseUrl = 'http://localhost:5440/masters/program';
 
 async function getList() {
-    let response = await fetch(baseUrl);
-    let data = await response.json();
-    console.log(data);
-    finalList = data['semesters'];
-    console.log(finalList);
-    //     data['users'].array.forEach(element => {
 
-    //     });
-    table = document.getElementById('listSemesters');
-    cnt = 0;
-    finalList.forEach(element => {
-        cnt++;
-        // roleTag = getRoleTag(element["admin_level"]);
-        table.insertAdjacentHTML('beforeend', `
+    var cred = {
+        my_id: "iib2019050",
+        my_level: 2
+    }
+    var options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(cred)
+    }
+    var response;
+    await fetch('http://localhost:5440/masters/semester?action=2', options)
+        .then(res => {
+            res.json()
+                .then(data => {
+                    console.log(data)
+                    response = data;
+                    finalList = response['semesters'];
+                    console.log(finalList);
+                    table = document.getElementById('listSemesters');
+                    table.innerHTML = "";
+                    cnt = 0;
+                    finalList.forEach(element => {
+                        cnt++;
+                        table.insertAdjacentHTML('beforeend', `
                                             <tr>
                                                 <td>
                                                     ${cnt}
                                                 </td>
                                                 <td>
-                                                    ${element['sem_ID']}
+                                                    ${element['semester_id']}
                                                 </td>
                                                 <td>
-                                                    ${element['sem_num']}
+                                                    ${element['semester_num']}
                                                 </td>
                                                 <td>
-                                                    ${element['branch_ID']}
+                                                    ${element['branch_id']}
                                                 </td>
                                             </tr>
-        `);
-    });
+                        `);
+                    });
+                })
+                .catch(err => console.log(`${err}`))
+        })
+        .catch(err => console.error(err))
 }
 
