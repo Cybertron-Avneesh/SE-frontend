3f86574d9c75fbd2423c1b7871d4df255a58730e
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2021-04-04 11:48:59 +0530

    Adds Fetch notification for each user

--- script/operating.get_notification.js
+++ script/operating.get_notification.js
@@ -1,35 +1,3 @@
-async function addNotification() {
-
-    var target = document.getElementById('inputTarget').value;
-    var msg = document.getElementById('inputNotifMsg').value;
-    if (!target) {
-        target = "";
-    }
-    var cred = {
-        my_id: "iib2019050",
-        my_level: 2,
-        enrollment_id: target,
-        description: msg
-    }
-    console.log(cred);
-    var options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(cred)
-    }
-    await fetch(`http://localhost:5440/student/notification?action=1`, options)
-        .then(res => {
-            if (res.status === 200) {
-                window.alert("Notification sent successfully!");
-            } else {
-                window.alert("Error sending notification. Please try again.");
-            }
-        })
-        .catch(err => console.error(err))
-}
-
 async function getNotification() {
 
     var cred = {
@@ -54,7 +22,6 @@ async function getNotification() {
                     listDiv = document.getElementById('notifDiv');
                     notifList = document.getElementById('notifList');
                     notifList.innerHTML = "";
-                    var li;
                     data['Notifications'].forEach(element => {
                         notifList.insertAdjacentHTML('beforeend', `
                         <li >
