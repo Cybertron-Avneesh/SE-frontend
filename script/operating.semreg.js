57f1bc87f22c7898f17aee569af24bb0bb43442f
Shyam Tayal <shyam.tayal@hotmail.com>
2021-04-29 15:24:25 +0530

    sem reg connection

---
+++ script/operating.semreg.js
@@ -0,0 +1,69 @@
+currUserObj = JSON.parse(window.localStorage.getItem("currUserObj"));
+myID = currUserObj["user_id"];
+myLevel = currUserObj["admin_level"];
+
+
+async function search_program() {
+
+    var cred = {
+        my_id: myID??"TEMPUSER",
+        my_level: myLevel??2,
+        program_id: document.getElementById('inputProgramID').value,
+    }
+    var options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(cred)
+    }
+    await fetch(`http://localhost:5440/student/create?action=2`, options)
+        .then(res => {
+            res.json()
+                .then(data => {
+                    console.log(data)
+                    response = data;
+                    // appending them to the list
+                    var regTable = document.getElementById('sem_reg_table');
+                    regTable.innerHTML = "";
+                    var cnt = 0;
+                    data[''].forEach(element => {
+                        cnt++;
+                        regTable.insertAdjacentHTML('beforeend',
+                            `
+                            <tr>
+                                  <td>
+                                        ${cnt}
+                                  </td>
+                                  <td>
+                                        ${element['enrollment_id']}
+                                  </td>
+                                  <td>
+                                        ${element['name']}
+                                  </td>
+                                  <td>
+                                        ${element['DOB']}
+                                  </td>
+                                  <td>
+                                        ${element['Semester']}
+                                  </td>
+                            </tr>
+                            `
+                        )
+                    });
+                    // notifList.insertAdjacentHTML('beforeend', li);
+
+                })
+                .catch(err => {
+                    console.log(err);
+                    document.getElementById("sem_reg_table").innerHTML = `
+                    <p class=" alert-danger" align="center" style="padding: 20px; font-size: 12px;">
+                        Error loading fee status.
+                    </p>
+                `;
+                })
+
+
+        })
+        .catch(err => console.error(err))
+}
